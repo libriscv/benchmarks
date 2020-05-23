9501b7f8aef9511397fdd73dffcd3a1dab94c8de
Alf-André Walla <fwsgonzo@hotmail.com>
2020-05-23 14:02:37 +0200

    Update Rust with more benchmarks

--- rvprogram/rustbin/build.sh
+++ rvprogram/rustbin/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cargo rustc --release --target riscv32imac-unknown-none-elf -- -C link-args="-e _start -static --undefined=fastexit --undefined=empty_function --undefined=test"
+cargo rustc --release --target riscv32imac-unknown-none-elf -- -C link-args="-e _start -static --undefined=fastexit --undefined=empty_function --undefined=test --undefined=test_args --undefined=test_maffs --undefined=test_print"
--- rvprogram/rustbin/src/main.rs
+++ rvprogram/rustbin/src/main.rs
@@ -64,3 +64,31 @@ pub unsafe extern "C" fn test(arg1: i32)
 	COUNTER += 1;
 	llvm_asm!("ebreak");
 }
+
+#[repr(C)]
+pub struct Value {
+	a: i32,
+	b: i64
+}
+#[naked]
+#[no_mangle]
+pub extern "C" fn test_args(a1: u32, a2: Value, a3: i32, a4: i32, a5: i32, a6: i32, a7: i32, a8: i32) -> i32
+{
+	if a1 == 0x876633F && a2.a == 222 && a2.b == 666 && a3 == 333
+	&& a4 == 444 && a5 == 555 && a6 == 666 && a7 == 777 && a8 == 888 {
+		return 666;
+	}
+	return a2.a;
+	return -1;
+}
+
+#[naked]
+#[no_mangle]
+pub extern "C" fn test_maffs(a1: i32, a2: i32) -> i32
+{
+	let a = a1 + a2;
+	let b = a1 - a2;
+	let c = a1 * a2;
+	let d = a1 / a2;
+	return a + b + c + d;
+}
